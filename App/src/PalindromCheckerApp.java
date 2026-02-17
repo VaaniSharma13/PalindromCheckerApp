da02cfd1d1eecd0c5dc5c15fc1509ad98c3ddc9d
VaaniSharma13 <vaanisharma1309@gmail.com>
2026-02-17 14:45:40 +0530

    vaani added VaaniSharma13

---
+++ App/src/PalindromCheckerApp.java
@@ -0,0 +1,31 @@
+/**
+ * MAIN CLASS - UseCasePalindromeApp
+ *
+ * Use Case 1 : Application Entry and Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker management System
+ *
+ * At this stage, the application:
+ * -Starts execution from the main() method
+ * -display a welcome message
+ * -Shows application version
+ *
+ * No palindrome logic is implemented here yet.
+ *
+ * the goal is to establish a clear startup flow.
+ *
+ * @vaani Developer
+ * @version 1.0
+ */
+
+public class PalindromCheckerApp {
+    /**
+     *
+     * @vaani args Command-line arguments
+     */
+    public static void main(String[] args){
+
+    }
+}
