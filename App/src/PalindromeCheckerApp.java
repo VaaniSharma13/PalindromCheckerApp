04a0f4857bb047fde2f850622737b7d15c9044ce
VaaniSharma13 <vaanisharma1309@gmail.com>
2026-02-24 14:53:55 +0530

    vaani added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -20,12 +20,27 @@
  * @version 1.0
  */
 
-public class PalindromCheckerApp {
+public class PalindromeCheckerApp {
     /**
      *
      * @vaani args Command-line arguments
      */
     public static void main(String[] args){
+
+
+        String word = "madam";
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
 
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# PalindromCheckerApp
+# PalindromeCheckerApp
 STEP - 
 Set up IntelliJ environment and then proceeded to create a login page for a Palindrome Checker App
+
+Added UC2 - reads input and checks for palindrome
